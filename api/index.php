d5146c1daaa7c898af5ad0a2d9262827fbb025e4
mac <azis@aziss-MacBook-Pro.local>
2026-06-04 22:28:39 +0700

    Fix Vercel deployment: use vercel-php runtime with proper configuration and env variables

--- api/index.php
+++ api/index.php
@@ -3,10 +3,6 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
-
 define('LARAVEL_START', microtime(true));
 
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
@@ -15,11 +11,8 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 
 require __DIR__.'/../vendor/autoload.php';
 
-try {
-    /** @var Application $app */
-    $app = require_once __DIR__.'/../bootstrap/app.php';
-    $app->handleRequest(Request::capture());
-} catch (Throwable $e) {
-    http_response_code(500);
-    echo '<pre>' . htmlspecialchars((string) $e, ENT_QUOTES, 'UTF-8') . '</pre>';
-}
+$_ENV['APP_BASE_PATH'] = dirname(__DIR__);
+
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+$app->handleRequest(Request::capture());
